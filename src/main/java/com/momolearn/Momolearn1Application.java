af4ceee7573ed72531336ca782c707a7f3b77354
noljis <popsol95@gmail.com>
2023-03-15 16:30:39 +0900

    first commit

---
+++ src/main/java/com/momolearn/Momolearn1Application.java
@@ -0,0 +1,13 @@
+package com.momolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Momolearn1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Momolearn1Application.class, args);
+	}
+
+}
